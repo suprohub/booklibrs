876780fa78dc4b73b4dca62fe68b453e4dceeee7
suprohub <suprohub@gmail.com>
2024-11-22 13:02:45 +0400

    commit

--- docs/sw.js
+++ docs/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './booklibrs/app.js',
-  './booklibrs/app_bg.wasm',
+  './booklibrs/booklibrs/app.js',
+  './booklibrs/booklibrs/app_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
